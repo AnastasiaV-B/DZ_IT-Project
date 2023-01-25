2c127d642a4ea55ccdec760ae0aea95071357cdb
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-25 12:55:34 +0300

    план действий

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,12 +6,47 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+// решение:
+// 1. Делаем перебор значений из исходного массива;
+// 2. Проверяем: длина строки меньше или равна 3;
+// 3. Если строка удовлетворяет условию кладем значение в новый массив;
+// 4. Повторяем пункты 2 и 3 до тех пор, пока не достигнем конца исходного массива;
+// 5. Возвращаем новый заполненый массив как результат.
 
-/* решения:
-Делаем перебор значений из исходного массива
-Проверяем каждое значение из массива на соответствие условию: длина строки меньше или равна трем
-Если строка удовлетворяет условию кладем значение в новый массив
-Повторяем пункты 2 и 3 до тех пор пока не достигнем конца исходного массива
-Возвращаем новый заполненый массив как результат /*
 
-
+using System;
+using static System.Console;
+Clear();
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+string[] FindLessThan(string[] input, int n)
+{
+    string[] output = new string[CountLessThan(input, n)];
+    for(int i = 0, j = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] AskArray()
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
